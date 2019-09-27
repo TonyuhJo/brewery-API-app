4ad7f484d88219a81770cd3a3c74022a4d7afe88
Tonya Stombaugh <tonya.stombaugh@gmail.com>
2019-09-27 10:57:26 -0700

    Apply mobile styling

--- script.js
+++ script.js
@@ -2,79 +2,80 @@ function showSearchStart(){
     console.log("showing search start");
     
     $('.search-form').html(`<form id="search-form" class="hidden">
-              <h3>Where would you like to search?</h3>
-              <label for="search-city">City: </label>
-              <input type="text" name="city-search" id="js-city-search" placeholder="Enter city name">
-              <label for="search-state">State: </label>
-              <select name="state" id="js-state-search">
-                <option value="alabama">AL</option>
-                <option value="alaska">AK</option>
-                <option value="arizona">AZ</option>
-                <option value="arkansas">AR</option>
-                <option value="california">CA</option>
-                <option value="colorado">CO</option>
-                <option value="connecticut">CT</option>
-                <option value="delaware">DE</option>
-                <option value="florida">FL</option>
-                <option value="georgia">GA</option>
-                <option value="hawaii">HI</option>
-                <option value="idaho">ID</option>
-                <option value="illinois">IL</option>
-                <option value="indiana">IN</option>
-                <option value="iowa">IA</option>
-                <option value="kansas">KS</option>
-                <option value="kentucky">KY</option>
-                <option value="louisiana">LA</option>
-                <option value="maine">ME</option>
-                <option value="maryland">MD</option>
-                <option value="massachusetts">MA</option>
-                <option value="michigan">MI</option>
-                <option value="minnesota">MN</option>
-                <option value="mississippi">MS</option>
-                <option value="missouri">MO</option>
-                <option value="montana">MT</option>
-                <option value="nebraska">NE</option>
-                <option value="nevada">NV</option>
-                <option value="new_hampshire">NH</option>
-                <option value="new_jersey">NJ</option>
-                <option value="new_mexico">NM</option>
-                <option value="new_york">NY</option>
-                <option value="north_carolina">NC</option>
-                <option value="north_dakota">ND</option>
-                <option value="ohio">OH</option>
-                <option value="oklahoma">OK</option>
-                <option value="oregon">OR</option>
-                <option value="pennsylvania">PA</option>
-                <option value="rhode_island">RI</option>
-                <option value="south_carolina">SC</option>
-                <option value="south_dakota">SD</option>
-                <option value="tennessee">TN</option>
-                <option value="texas">TX</option>
-                <option value="utah">UT</option>
-                <option value="vermont">VT</option>
-                <option value="virginia">VA</option>
-                <option value="washington">WA</option>
-                <option value="west_virginia">WV</option>
-                <option value="wisconsin">WI</option>
-                <option value="wyoming">WY</option>
-              </select>
-              <h3>Have a specific type of brewery in mind?</h3>
-              <select name="search-type" id="js-type-search">
-                <option value="">Show me all of them</option>
-                <option value="micro">Micro</option>
-                <option value="regional">Regional</option>
-                <option value="brewpub">Brewpub</option>
-                <option value="large">Large</option>
-                <option value="planning">Planning</option>
-                <option value="bar">Bar</option>
-                <option value="contract">Contract</option>
-                <option value="proprietor">Proprietor</option>
-              </select><br><br>
-              <input type="submit" value="Search" class="js-submit-search">
-            </form>
-            <section class="results hidden">
-            </section>`);
-  }
+        <img src="images/brewHoundLogo.png" alt="Brew Hound logo" class="logo2">
+        <h3>Where would you like to search?</h3>
+        <div class="city-state"><label for="search-city" class="search-city">City: </label>
+        <input type="text" name="city-search" id="js-city-search" placeholder="Enter city name">
+        <label for="search-state" class="search-state">State: </label>
+        <select name="state" id="js-state-search">
+            <option value="alabama">AL</option>
+            <option value="alaska">AK</option>
+            <option value="arizona">AZ</option>
+            <option value="arkansas">AR</option>
+            <option value="california">CA</option>
+            <option value="colorado">CO</option>
+            <option value="connecticut">CT</option>
+            <option value="delaware">DE</option>
+            <option value="florida">FL</option>
+            <option value="georgia">GA</option>
+            <option value="hawaii">HI</option>
+            <option value="idaho">ID</option>
+            <option value="illinois">IL</option>
+            <option value="indiana">IN</option>
+            <option value="iowa">IA</option>
+            <option value="kansas">KS</option>
+            <option value="kentucky">KY</option>
+            <option value="louisiana">LA</option>
+            <option value="maine">ME</option>
+            <option value="maryland">MD</option>
+            <option value="massachusetts">MA</option>
+            <option value="michigan">MI</option>
+            <option value="minnesota">MN</option>
+            <option value="mississippi">MS</option>
+            <option value="missouri">MO</option>
+            <option value="montana">MT</option>
+            <option value="nebraska">NE</option>
+            <option value="nevada">NV</option>
+            <option value="new_hampshire">NH</option>
+            <option value="new_jersey">NJ</option>
+            <option value="new_mexico">NM</option>
+            <option value="new_york">NY</option>
+            <option value="north_carolina">NC</option>
+            <option value="north_dakota">ND</option>
+            <option value="ohio">OH</option>
+            <option value="oklahoma">OK</option>
+            <option value="oregon">OR</option>
+            <option value="pennsylvania">PA</option>
+            <option value="rhode_island">RI</option>
+            <option value="south_carolina">SC</option>
+            <option value="south_dakota">SD</option>
+            <option value="tennessee">TN</option>
+            <option value="texas">TX</option>
+            <option value="utah">UT</option>
+            <option value="vermont">VT</option>
+            <option value="virginia">VA</option>
+            <option value="washington">WA</option>
+            <option value="west_virginia">WV</option>
+            <option value="wisconsin">WI</option>
+            <option value="wyoming">WY</option>
+        </select></div>
+        <h3>Have a specific type of brewery in mind?</h3>
+        <select name="search-type" id="js-type-search">
+            <option value="">Show me all of them</option>
+            <option value="micro">Micro</option>
+            <option value="regional">Regional</option>
+            <option value="brewpub">Brewpub</option>
+            <option value="large">Large</option>
+            <option value="planning">Planning</option>
+            <option value="bar">Bar</option>
+            <option value="contract">Contract</option>
+            <option value="proprietor">Proprietor</option>
+        </select><br><br>
+        <input type="submit" value="Search" class="js-submit-search">
+        </form>
+        <section class="results hidden">
+        </section>`);
+}
     
 function renderSearchStart(){
     console.log("rendering search start");
@@ -126,15 +127,15 @@ function displayResults(responseJson){
     
     if(responseJson.length > 0){
   
-      $('.results').append(`<h2>Results</h2>`)
+      $('.results').append(`<h2 class="results-start">Results</h2>`)
       for(let i = 0; i < responseJson.length; i++){
     
         let address = `${responseJson[i].street}<br>${responseJson[i].city}, ${responseJson[i].state}<br>${responseJson[i].postal_code}`;
     
         $('.results').append(`<li><h3>${responseJson[i].name}</h3>
-        <p>Address: ${address}</p>
+        <p>${address}</p><br>
         <p>Phone: ${responseJson[i].phone}</p>
-        <p><a href="${responseJson[i].website_url}" target='_blank'>${responseJson[i].website_url}</a></p>`)
+        <p><a href="${responseJson[i].website_url}" target='_blank' class="site-url">${responseJson[i].website_url.slice(7)}</a></p>`)
       };
     } else {
       $('.results').append(`Sorry! It doesn't look like we have any results that match your search criteria. You may want to adjust your search and try again.`)
